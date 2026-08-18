0f678ea49470d270b031c5728709f84532ef4cbe
indodev01 <indomanegement@gmail.com>
2026-08-18 06:55:35 +0530

    Connect Firebase Realtime Database

--- frontend/auth/firebase-config.js
+++ frontend/auth/firebase-config.js
@@ -1,5 +1,5 @@
 // Firebase configuration for Indo Dev.
-// These values identify the Firebase web app.
+// These values identify the Firebase web app and Realtime Database.
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyARtWJ46hs6HEVYAASm19r55EwbycgFY2I',
@@ -8,8 +8,8 @@ export const firebaseConfig = {
   storageBucket: 'indo-dev-d150f.firebasestorage.app',
   messagingSenderId: '1055323979335',
   appId: '1:1055323979335:web:ebd7217748446c21b97b62',
-  measurementId: 'G-9KD7XEJEYJ'
+  measurementId: 'G-9KD7XEJEYJ',
+  databaseURL: 'https://indo-dev-d150f-default-rtdb.asia-southeast1.firebasedatabase.app/'
 };
 
-// Add databaseURL here after the Realtime Database is created.
-export const realtimeDatabaseUrl = '';
+export const realtimeDatabaseUrl = firebaseConfig.databaseURL;
